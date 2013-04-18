bd730e97a1ee864ccc04dc6156170e1e69bb4766
jpsirois <email@jpsirois.com>
2013-04-18 10:33:42 -0400

    Fix useless & wrong function call (legacy stuff from another extension)

--- src/javascript/included.js
+++ src/javascript/included.js
@@ -2,8 +2,3 @@ resizeThumbnails = function() {
   $('.thumbImg').each(function(){  $(this).attr('src', $(this).attr('src').replace('~~48_14.JPG', '~~48_35.JPG')) })
 }
 resizeThumbnails()
-
-// TODO @jpsirois: This is an Ugly way to be sure all the stuff is loaded
-window.setTimeout(removeAds, 1000)
-window.setTimeout(removeAds, 3000)
-window.setTimeout(removeAds, 5000)
