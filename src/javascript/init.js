37888db5df41a2273b7767daf513cef5dfd10fa8
jpsirois <email@jpsirois.com>
2013-04-18 10:38:20 -0400

    Clean init.js

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -1,6 +1,7 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('javascript/included.js');
+// Add extension JS in page
+var s = document.createElement('script')
+s.src = chrome.extension.getURL('javascript/included.js')
 s.onload = function() {
-    this.parentNode.removeChild(this);
-};
-(document.head||document.documentElement).appendChild(s);
+    this.parentNode.removeChild(this)
+}
+(document.head||document.documentElement).appendChild(s)
